9ecc3e92f27b24f89b86eb88a9986b0bb37f046d
janipoddi <157234959+janipoddi@users.noreply.github.com>
2024-11-24 18:41:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? '': process.env.SESSION_ID,
 GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'janipoddi': process.env.GITHUB_USERNAME,
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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': process.env.GITHUB_AUTH_TOKEN,
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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': process.env.GITHUB_AUTH_TOKEN,
 BOT_NUMBER: process.env.BOT_NUMBER === undefined ? '94740952096': process.env.BOT_NUMBER,
 OMDB_API_KEY: process.env.OMDB_API_KEY="76cb7f39",
 MONGODB_URI :process.env.MONGODB_URI === undefined ? 'mongodb+srv://yasiyaytlk:yasi123@test1.i7ezb.mongodb.net/' : process.env.MONGODB_URI
